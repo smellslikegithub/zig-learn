729f2c010869a6f037a5022481b10d60f155070b
Ashish Husain <ashish.m.husain@gmail.com>
2026-05-22 15:54:26 +0200

    Basics of fucntion usage etc

--- hello-world.zig
+++ hello-world.zig
@@ -1,6 +1,18 @@
 
+const std:type = @import("std");
+
 // Function example in Zig
+// Function names: camelCase
+pub fn addU8s(num1: u8, num2:u8) u16 {
+    
+    return num1 + num2;
+}
 
 pub fn main() void {
-    @import("std").debug.print("Hello World\n", .{});
+    // Custom types: TitleCase
+
+    // Varibales: snake_case
+    const special_number: u8 = 100;
+
+    std.debug.print("Hello World {}\n ", .{(special_number)});
 }
